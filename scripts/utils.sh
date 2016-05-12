79ebe14a9cf6c66b63dddd7be6504000fe855c14
Jorge Morante <jorge@morante.eu>
2016-05-12 10:45:38 +0200

    silent pane-exec to avoid history pollution

--- scripts/utils.sh
+++ scripts/utils.sh
@@ -22,6 +22,6 @@ function pane_exec() {
   local pane_id=$1
   local pane_command=$2
 
-  tmux send-keys -t $pane_id "$pane_command"
+  tmux send-keys -t $pane_id " $pane_command"
   tmux send-keys -t $pane_id Enter
 }
