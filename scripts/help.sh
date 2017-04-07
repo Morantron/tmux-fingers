b61ea77cd20fc614e16b169796ae645f845bf326
Jorge Morante <jorge@morante.eu>
2017-04-07 09:26:04 +0200

    add version to help screen

--- scripts/help.sh
+++ scripts/help.sh
@@ -3,12 +3,16 @@
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source $CURRENT_DIR/utils.sh
 
+pushd $CURRENT_DIR
+FINGERS_VERSION=$(git describe --tags | sed "s/-.*//g")
+popd
+
 function show_help() {
   fingers_pane_id=$1
   clear_screen "$fingers_pane_id"
 
 cat << ENDOFHELP
-tmux-fingers help:
+tmux-fingers ( $FINGERS_VERSION ) help:
 
 - a-z: yank a highlighted hint
 - <space>: toggle compact hints on/off
