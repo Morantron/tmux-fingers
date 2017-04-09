0d69d6412f584755de419a0b73d58e6cf9df24eb
Jorge Morante <jorge@morante.eu>
2017-04-09 17:40:15 +0200

    improving health cheack and adding at least gawk 4

--- test/provisioning/ubuntu.sh
+++ test/provisioning/ubuntu.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
-apt-get remove -y tmux
-apt-get install -y libevent-dev libncurses5-dev expect fish
-apt-get install -y gawk
+sudo aptitude update
+sudo aptitude install -y expect fish gawk
 
 useradd -m -p "$(perl -e "print crypt('fishman','sa');")" -s "/usr/bin/fish" fishman
 
 wget https://github.com/tmux/tmux/releases/download/2.2/tmux-2.2.tar.gz
 
+# install tmux from source
+sudo aptitude remove -y tmux
+sudo aptitude install -y libevent-dev libncurses5-dev
 tar xvzf tmux-2.2.tar.gz
 cd tmux-2.2/ || echo "Could not find tmux-2.2/ folder" || exit 1
 
