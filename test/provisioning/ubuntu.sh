191a2553a5ee7925b0abc6bd12d5286e61788d30
Jorge Morante <jorge@morante.eu>
2018-03-11 20:14:36 +0100

    bump to tmux 2.6 in ubuntu VM provisioning

--- test/provisioning/ubuntu.sh
+++ test/provisioning/ubuntu.sh
@@ -5,13 +5,13 @@ sudo aptitude install -y fish gawk
 
 useradd -m -p "$(perl -e "print crypt('fishman','sa');")" -s "/usr/bin/fish" fishman
 
-wget https://github.com/tmux/tmux/releases/download/2.2/tmux-2.2.tar.gz
+wget https://github.com/tmux/tmux/releases/download/2.6/tmux-2.6.tar.gz
 
 # install tmux from source
 sudo aptitude remove -y tmux
 sudo aptitude install -y libevent-dev libncurses5-dev
-tar xvzf tmux-2.2.tar.gz
-cd tmux-2.2/ || echo "Could not find tmux-2.2/ folder" || exit 1
+tar xvzf tmux-2.6.tar.gz
+cd tmux-2.6/ || echo "Could not find tmux-2.6/ folder" || exit 1
 
 ./configure
 make
