f529b4d1309a5d756eeb685f519990c7e95ecdd2
Jorge Morante <jorge@morante.eu>
2018-03-11 20:48:28 +0100

    add TODO regarding tput in test/provisioning/bsd.sh

--- test/provisioning/bsd.sh
+++ test/provisioning/bsd.sh
@@ -3,5 +3,7 @@
 pkg install -y bash tmux fish gawk
 chsh -s bash vagrant
 
+# TODO tput is broken in BSD, /usr/local/bin/tput should be used instead
+
 echo "fishman" | pw user add -n fishman -h 0 -s "/usr/local/bin/fish"
-echo "run /home/vagrant/shared/tmux-fingers.tmux" > .tmux.conf
+echo "alias tput=/usr/local/bin/tput" > .bash_profile
