ce77e07ada3cf40c669a98bf7940d197d3ca2247
Jorge Morante <jorge@morante.eu>
2020-01-05 12:18:39 +0100

    update help section

--- scripts/help.sh
+++ scripts/help.sh
@@ -14,10 +14,20 @@ function show_help() {
 cat << ENDOFHELP
 tmux-fingers ( $FINGERS_VERSION ) help:
 
-- a-z: yank a highlighted hint
-- <space>: toggle compact hints on/off
-- <Ctrl-C>: exit [fingers] mode
-- <esc>: exit help or [fingers] mode
-- ?: show/hide this help
+- a-z:           copies selected match to the clipboard
+- <ctrl> + a-z:  copies selected match to the clipboard and triggers
+                 @fingers-ctrl-action. By default it triggers :open: action, which is useful
+                 for opening links in the browser for example.
+- <shift> + a-z: copies selected match to the clipboard and triggers
+                 @fingers-shift-action. By default it triggers :paste: action, which
+                 automatically pastes selected matches.
+- <alt> + a-z:   copies selected match to the clipboard and triggers
+                 @fingers-alt-action. There is no default, configurable by the user.
+- <space>:       toggle compact hints on/off
+- <Tab>:         toggle multi mode. First press enters multi mode, which allows
+                 to select multiple matches. Second press will exit with the selected matches
+                 copied to the clipboard.
+- ?:             show/hide this help
+- <Ctrl-C>, <esc> or q: exit [fingers] mode
 ENDOFHELP
 }
