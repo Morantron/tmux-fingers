2795f8aaa7e46413c11b974c58a8d7b30fa28f50
Jorge Morante <jorge@morante.eu>
2026-01-11 18:54:44 +0100

    add running_in_specs? flag

--- src/fingers.cr
+++ src/fingers.cr
@@ -1,9 +1,13 @@
 require "./fingers/logger"
 require "./fingers/cli"
 
+def running_in_specs? : Bool
+  {{ @type.has_constant?("Spec") }}
+end
+
 module Fingers
   VERSION = {{ %(#{`shards version`.chomp}) }}
 
   cli = Cli.new
-  cli.run
+  cli.run unless running_in_specs?
 end
